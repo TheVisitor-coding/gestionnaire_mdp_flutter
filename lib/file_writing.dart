2438ac5a6d37a01c374999d1daca6cbc1cc04db7
Mitrix44 <dimitri.beziau@gmail.com>
2024-05-20 16:32:21 +0200

    Création du fichier data.json si il n'éxiste pas

--- lib/file_writing.dart
+++ lib/file_writing.dart
@@ -5,6 +5,9 @@ class FileActions {
   var file = File("./data.json");
 
   void writeOnFile(content) {
+    if (!file.existsSync()) {
+      file.createSync();
+    }
     file.writeAsString(jsonEncode(content).toString());
   }
 }
