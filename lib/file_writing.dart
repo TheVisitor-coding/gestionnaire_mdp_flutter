2be371807796d6efa47f04af064acb9430dc9d67
MatteoRossi_Dev <matteorossiroy.pro@gmail.com>
2024-05-21 09:01:10 +0200

    Add func crypt and decrypt data

--- lib/file_writing.dart
+++ lib/file_writing.dart
@@ -1,13 +1,18 @@
 import 'dart:io';
 import 'dart:convert';
+import 'password_manager.dart';
 
 class FileActions {
   var file = File("./data.json");
 
-  void writeOnFile(content) {
+  void writeOnFile(content) async {
     if (!file.existsSync()) {
       file.createSync();
     }
-    file.writeAsString(jsonEncode(content).toString());
+    final jsonData = jsonEncode(content);
+    final cryptedData = await PasswordManager().encryptData(jsonData);
+    await file.writeAsString(jsonEncode(cryptedData).toString());
+    final decryptedData = await PasswordManager().decryptData(cryptedData);
+    print("Données décryptées : $decryptedData");
   }
 }
--- lib/password_manager.dart
+++ lib/password_manager.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:cryptography/cryptography.dart';
+import 'dart:typed_data';
 
 class PasswordManager {
   static const String _dataFile = 'data.txt';
   static const int _pbkdf2Iterations = 100000;
+
+  static SecretKey? _masterKey;
 
   final Pbkdf2 pbkdf2 = Pbkdf2(
     macAlgorithm: Hmac.sha256(),
@@ -20,6 +23,8 @@ class PasswordManager {
       secretKey: secretKey,
       nonce: nonce,
     );
+
+    _masterKey = newSecretKey;
 
     final hash = await newSecretKey.extractBytes();
     final hashString = base64.encode(nonce + hash);
@@ -46,6 +51,8 @@ class PasswordManager {
 
     final newKey = await newSecretKey.extractBytes();
 
+    _masterKey = newSecretKey;
+
     return _compareLists(storedKey, newKey);
   }
 
@@ -60,5 +67,47 @@ class PasswordManager {
       if (list1[i] != list2[i]) return false;
     }
     return true;
+  }
+
+  Future<String> encryptData(String data) async {
+    if (_masterKey == null) {
+      throw Exception('Master password not set');
+    }
+    final algorithm = AesGcm.with256bits();
+    final nonce = algorithm.newNonce();
+
+    final encrypted = await algorithm.encrypt(
+      utf8.encode(data),
+      secretKey: _masterKey!,
+      nonce: nonce,
+    );
+
+    final combined = nonce + encrypted.cipherText + encrypted.mac.bytes;
+    return base64.encode(Uint8List.fromList(combined));
+  }
+
+  Future<String> decryptData(String data) async {
+    if (_masterKey == null) {
+      throw Exception('Master password not set');
+    }
+    final algorithm = AesGcm.with256bits();
+    final decoded = base64.decode(data);
+
+    final nonce = decoded.sublist(0, 12);
+    final cipherText = decoded.sublist(12, decoded.length - 16);
+    final mac = Mac(decoded.sublist(decoded.length - 16));
+
+    final secretBox = SecretBox(
+      cipherText,
+      nonce: nonce,
+      mac: mac,
+    );
+
+    final decrypted = await algorithm.decrypt(
+      secretBox,
+      secretKey: _masterKey!,
+    );
+
+    return utf8.decode(decrypted);
   }
 }
